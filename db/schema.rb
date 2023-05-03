45bebec75090b86aecd2370dfece1b5be338f3fa
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-05-03 21:05:22 +0900

    スペルミス修正

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_05_03_054805) do
   create_table "boards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
-    t.string "bodym", null: false
+    t.string "body", null: false
     t.boolean "status", null: false
     t.bigint "list_id", null: false
     t.datetime "created_at", null: false
