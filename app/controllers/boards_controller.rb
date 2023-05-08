2f216da98157d61c5e016c67813030fa7f5467ed
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-05-08 21:10:15 +0900

    コントローラの記述

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,14 +5,22 @@ class BoardsController < ApplicationController
 
   def new
     @board = Board.new
-    boards = @board.list.build
+    # @list = List.new
+    # @task = Task.new
+    boards = @board.lists.build
     boards.tasks.build
   end
 
   def create
     @board = Board.new(board_params)
-    if @board.valid?
-      @board.save!
+    # @list = List.new(list_params)
+    # @task = Task.new(task_params)
+    if @board.save!
+      # @list.save!
+      # if @list.save!
+      #   @task.save!
+      # end
+      redirect_to boards_path, notice: 'Board was successfully created.'
     else
       render :new
     end
@@ -20,9 +28,17 @@ class BoardsController < ApplicationController
 
   private
     def board_params
-      params.require(:board).permit(:title, :body,:status,list_attributes: [:title, 
+      params.require(:board).permit(:title, :body,lists_attributes: [:title, 
         tasks_attributes: [:title, :body, :diffculty_level, :is_solo]
         ]
       )
     end
+
+    # def list_params
+    #   params.require(:board).permit(list:[:title])
+    # end
+
+    # def task_params
+    #   params.require(:board).permit(task:[:title, :body, :diffculty_level, :is_solo])
+    # end
 end
