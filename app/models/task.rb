1b711c2618a8cc614b75d62d75cfe382c82bc260
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-05-11 20:50:02 +0900

    taskモデルにバリデーションをかける

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :list, optional: true
   accepts_nested_attributes_for :list
+  validates :title, presence: true, length: {maximum: 15}
+  validates :body, length: {maximum: 1800}
+  # validates :difficulty_level, presence: true, length: {maximum: 5}  まだ実装していないためコメントアウト
 end
