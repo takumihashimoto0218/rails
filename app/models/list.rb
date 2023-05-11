37c5d2b4c8c4dc158bb2ead55ad59de5370d213b
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-05-11 20:57:41 +0900

    スペルミス修正

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,5 @@ class List < ApplicationRecord
   accepts_nested_attributes_for :tasks
   belongs_to :board, optional: true
   accepts_nested_attributes_for :board
-  validates :title, prsence: true, length: {maximum: 30}
+  validates :title, presence: true, length: {maximum: 30}
 end
