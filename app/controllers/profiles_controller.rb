aaf3ad9f5d307442f23a48b7e1e6393cfbac42ae
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-05-14 22:16:51 +0900

    profileコントローラの作成

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,24 @@
+class ProfilesController < ApplicationController
+  
+  def show
+    @profile = Profile.find(params[:id])
+  end
+  
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save!
+      redirect_to boards_path
+    else
+      render :new
+    end  
+  end
+
+  private
+    def profile_params
+      params.require(:profile).permit(:name, :self_introduction)
+    end
+end
