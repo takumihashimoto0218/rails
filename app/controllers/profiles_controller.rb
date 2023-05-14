f2c254975e2dfef0dc3ae4213ef5ca46a95960ce
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-05-15 00:00:56 +0900

    編集できるようにコントローラを編集

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
-  
+  before_action :set_profile, only: [:show, :edit, :update]
+
   def show
-    @profile = current_user.profile
   end
   
   def new
@@ -11,14 +11,29 @@ class ProfilesController < ApplicationController
   def create
     @profile = current_user.build_profile(profile_params)
     if @profile.save!
-      redirect_to boards_path
+      redirect_to boards_path, notice: 'プロフィールを登録しました'
     else
       render :new
     end  
   end
 
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profile_path, notice: '更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
     def profile_params
       params.require(:profile).permit(:name, :self_introduction)
+    end
+
+    def set_profile
+      @profile = current_user.profile
     end
 end
