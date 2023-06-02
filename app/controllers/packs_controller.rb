1849cb0fe24f343bfb47267dd27cad7e52ebd4a4
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-03 02:45:07 +0900

    コントローラに処理を記述

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -6,4 +6,22 @@ class PacksController < ApplicationController
   def show
     @pack = Pack.find(params[:id])
   end
+
+  def new
+    @pack = Pack.new
+  end
+
+  def create
+    @pack = Pack.new(pack_params)
+    if @pack.save!
+      redirect_to packs_path, notice: "投稿しました"
+    else
+      render :new
+    end  
+  end
+
+  private 
+    def pack_params
+      params.require(:pack).permit(:name, :packdetail_id)
+    end
 end
