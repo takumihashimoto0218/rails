7e3f6e88063727ef6766c03578843315d8df73af
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-09 01:57:24 +0900

    draft

---
+++ app/controllers/packdetails_controller.rb
@@ -0,0 +1,29 @@
+require "net/http"
+require "uri"
+require "json"
+
+class Pack::PackdetailsController < ApplicationController
+  def index
+    page = params[:page] || 1
+
+    uri = URI("https://hanatane.jp/api/v1/topics")
+    uri.query = URI.encode_www_form({ page: page })
+
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+
+    begin
+      request = Net::HTTP::Get.new(uri)
+      response = https.request(request)
+
+      if response.is_a?(Net::HTTPSuccess)
+        @topics = JSON.parse(response.body)["topics"]
+        render json: @topics
+      else
+        render json: { error: "There was an error retrieving the plans." }
+      end
+    rescue StandardError => e
+      render json: { error: "An unexpected error occurred: #{e.message}" }
+    end
+  end
+end
