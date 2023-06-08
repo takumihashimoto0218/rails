f10716fd1805fd6052fe2802fdd6eb36d3b54068
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-09 01:55:36 +0900

    migration変更

--- db/migrate/20230519061809_create_packs.rb
+++ db/migrate/20230519061809_create_packs.rb
@@ -2,7 +2,7 @@ class CreatePacks < ActiveRecord::Migration[7.0]
   def change
     create_table :packs do |t|
       t.string :name,  null: false
-      t.bigint :packdetail_id, null: false
+      t.bigint :packdetail_id
 
       t.timestamps
     end
