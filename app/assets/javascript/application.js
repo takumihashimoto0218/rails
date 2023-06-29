3f9394c7d65063248a87decce5aa143a4688fabc
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-29 21:59:14 +0900

    draft

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -11,6 +11,13 @@ window.$ = jquery
 //   });
 // });
 
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import '@fortawesome/fontawesome-free'
+library.add(fas, far, fab)
+
 $(function(){
   alert("jQuery 動いた！")
 })
