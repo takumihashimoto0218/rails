960044a8268356be5d970ccfc893baa1ef27ac1c
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-07-04 16:13:10 +0900

    fontawesomeを追加

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,22 +1,6 @@
 import jquery from "jquery"
 window.$ = jquery
-// import '@hotwired/turbo-rails';
-// import 'controllers';
-// import 'favorites/create'
-// import 'favorites/destroy'
-
-// $(document).on('turbolinks:load', function() {
-//   $('#favorite-link, #unfavorite-link').on('ajax:success', function(event) {
-//     $(this).closest('.board').find('.favorite-count').text(event.detail[0].favorite_count);
-//   });
-// });
-
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { library } from "@fortawesome/fontawesome-svg-core";
-import '@fortawesome/fontawesome-free'
-library.add(fas, far, fab)
+import "@fortawesome/fontawesome-free"
 
 $(function(){
   alert("jQuery 動いた！")
