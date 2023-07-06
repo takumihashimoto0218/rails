5cce1cc5e7b9040c9200dab01442513a77634276
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-07-06 21:18:50 +0900

    controller編集

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -9,29 +9,28 @@ class PacksController < ApplicationController
   
   def show
     @pack = Pack.find(params[:id])
-    # binding.pry
-    uri = URI("https://hanatane.jp/api/v1/topics/#{@pack.packdetails.first.topic_id}")
-    # uri.query = URI.encode_www_form()
-
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = true
-
+    uris = @pack.packdetails.map { |packdetail| URI("https://hanatane.jp/api/v1/topics/#{packdetail.topic_id}") }
+    @topics = []
+  
     begin
-      request = Net::HTTP::Get.new(uri)
-      response = https.request(request)
-
-      if response.is_a?(Net::HTTPSuccess)
-        @topics = JSON.parse(response.body)["topic"]
-      else
-        flash[:alert] = "There was an error retrieving the topics."
-        @topics = []
+      uris.each do |uri|
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+  
+        request = Net::HTTP::Get.new(uri)
+        response = https.request(request)
+  
+        if response.is_a?(Net::HTTPSuccess)
+          @topics << JSON.parse(response.body)["topic"]
+        else
+          flash[:alert] = "There was an error retrieving the topics."
+        end
       end
     rescue StandardError => e
       flash[:alert] = "An unexpected error occurred: #{e.message}"
-      @topics = []
     end
-    binding.pry
   end
+    
 
   def new
     @pack = Pack.new
