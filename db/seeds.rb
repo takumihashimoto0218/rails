18eb7143d3c977aa18f4892da63f93eaa4e11754
sugisoichiro <sugisocedar@gmail.com>
2023-07-19 15:10:25 +0900

    途中

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@ User.create!(
     { name: "test4", email: "test4@gamil.com", password: "testtest"}
   ]
 )
+
+Profile.create(
+  [
+    { name: "test1", user_id: 1, self_introduction: "私はテストユーザーです。よろしくお願いします。"},
+    { name: "test1", user_id: 2, self_introduction: "私はテストユーザーです。よろしくお願いします。"},
+    { name: "test1", user_id: 3, self_introduction: "私はテストユーザーです。よろしくお願いします。"},
+    { name: "test1", user_id: 4, self_introduction: "私はテストユーザーです。よろしくお願いします。"}
+  ]
+)
