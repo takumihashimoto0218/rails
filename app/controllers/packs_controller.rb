a810c05b6c8022685b369239039449218e4f808e
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-08-06 17:14:56 +0900

    controllerの編集

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -3,32 +3,16 @@ require "uri"
 require "json"
 
 class PacksController < ApplicationController
+ 
   before_action :set_pack, only: [:show, :edit, :update]
+  # before_action :set_topic, only: [:show]
+ 
   def index
     @packs = Pack.all
   end
   
   def show
-    uris = @pack.packdetails.map { |packdetail| URI("https://hanatane.jp/api/v1/topics/#{packdetail.topic_id}") }
-    @topics = []
-  
-    begin
-      uris.each do |uri|
-        https = Net::HTTP.new(uri.host, uri.port)
-        https.use_ssl = true
-  
-        request = Net::HTTP::Get.new(uri)
-        response = https.request(request)
-  
-        if response.is_a?(Net::HTTPSuccess)
-          @topics << JSON.parse(response.body)["topic"]
-        else
-          flash[:alert] = "There was an error retrieving the topics."
-        end
-      end
-    rescue StandardError => e
-      flash[:alert] = "An unexpected error occurred: #{e.message}"
-    end
+    @topics = PackWrapper.fetch_topics(@pack)
   end
     
 
@@ -69,5 +53,13 @@ class PacksController < ApplicationController
 
     def set_pack
       @pack = Pack.find(params[:id])
+    end
+
+    def set_topic
+      begin
+        @topics = PackWrapper.get_topic(uris)
+      rescue StandardError => e
+        flash[:alert] = e.message
+      end
     end
 end
