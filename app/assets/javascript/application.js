a92a89567a327926d6c574f8736627853d3b520b
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-08-27 04:38:08 +0900

    draft

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,8 +1,12 @@
 import jquery from "jquery"
+// import { Application } from "stimulus"
+// import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+// const application = Application.start()
+// const context = require.context(".", true, /\.js$/)
+// application.load(definitionsFromContext(context))
+// require("cocoon-js")
 window.$ = jquery
 import "@fortawesome/fontawesome-free"
-
-$(function(){
-  alert("jQuery 動いた！")
-})
-console.log("hello")
+require("jquery")
+require("@nathanvda/cocoon")
