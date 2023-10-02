faed2fe092b2941ee825ce8332dacc5a198bbb98
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-10-02 23:56:23 +0900

    不要なコメント削除

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,19 +1,3 @@
-// // history.replaceState("","",location.href + "/new");
-// // ページのロード時に実行される関数
-// window.addEventListener('DOMContentLoaded', () => {
-//   // ボタンやリンクなどの対象要素を取得
-//   const changeURLButton = document.getElementById('change-url-button');
-
-//   // ボタンがクリックされた際の処理
-//   changeURLButton.addEventListener('click', () => {
-//     // 新しいURLを設定（/boards/new）
-//     const newURL = '/boards/new';
-    
-//     // ブラウザの履歴を変更してURLを更新
-//     window.history.pushState({}, '', newURL);
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
   var currentURL = location.href;
   var newURL = currentURL.split('?')[0];
