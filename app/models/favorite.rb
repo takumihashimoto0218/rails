0d9aae474c4cacc2fb4e9b6d4029aab036b76579
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-11-17 14:37:06 +0900

    いいね一覧から検索ができるように

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,21 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :board
   validates :board_id, uniqueness: { scope: :user_id }
+
+  def self.ransackable_attributes(auth_object = nil)
+    super + ['board_title']
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    super + ['board']
+  end
+
+  def self.ransackable_scopes(auth_object = nil)
+    %i[search_board_title]
+  end
+
+  scope :search_board_title, ->(title) {
+    joins(:board).where('boards.title LIKE ?', "%#{title}%")
+  }
 end
+
